97fcc8def51a89765265bbe407d6a2b4847103d0
Pau <36511149+ptaltavull@users.noreply.github.com>
2022-09-21 17:04:00 +0200

    Create JavaSubstring.java

---
+++ java/strings/JavaSubstring.java
@@ -0,0 +1,20 @@
+package java.strings;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class JavaSubstring {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+
+        String result = S.substring(start, end);
+        System.out.println(result);
+    }
+}
