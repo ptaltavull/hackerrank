f72292e88b1eca7aa9fa7f412388dd2fe67cdc45
Pau <36511149+ptaltavull@users.noreply.github.com>
2022-09-30 12:22:56 +0200

    Create JavaAnagrams.java

---
+++ java/strings/JavaAnagrams.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    static boolean isAnagram(String a, String b) {
+        // Complete the function
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        
+        if(a.length() != b.length()) return false;
+        
+        char[] charArrA = a.toCharArray();
+        char[] charArrB = b.toCharArray();
+        
+         java.util.Arrays.sort(charArrA);
+         java.util.Arrays.sort(charArrB);
+        
+        if( java.util.Arrays.equals(charArrA, charArrB)) return true;
+        
+        return false;
+    }
+
+    public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
